51f323eb07d980fac9612a154f321bd65f8c32ca
Will Smith <will.smith@preplaysports.com>
2013-10-21 10:49:01 -0400

    Trying to get rid of modified content

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ WatchIt::Application.routes.draw do
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "log_in" => "sessions#new", :as => "log_in"
   get "sign_up" => "users#new", :as => "sign_up"
-  root :to => "users#new"
+  root :to => "home#index"
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
 end
