505da3c77df8f47c71d3ff96e4e8c078d990e399
Will Smith <will.smith@preplaysports.com>
2013-10-24 09:21:16 -0400

    added # config.assets.initialize_on_precompile = false

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,3 @@
-# config.assets.initialize_on_precompile = false
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
@@ -53,6 +52,8 @@ module WatchIt
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
+
+    config.assets.initialize_on_precompile = false
 
     # Enable the asset pipeline
     config.assets.enabled = true
